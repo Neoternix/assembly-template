bbca45f5c1675a6ceee57f777d850ff3114adfb6
Neosake <70122910+Neosake@users.noreply.github.com>
2022-10-12 20:00:37 -0500

    Create main.s

---
+++ main.s
@@ -0,0 +1,12 @@
+; Implement Initialized Data or Constants. ;
+.section .bss
+
+; Implement Uninitialized Data Variable's. ;
+.section .data
+
+; Main Implementation. ;
+.section .text
+    .global _main
+
+_main:
+    ; implementation
